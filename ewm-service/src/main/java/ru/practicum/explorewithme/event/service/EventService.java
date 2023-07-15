94cdaabf46d0cb03a34facbb36e3ab47247b4771
MelTrevelyan <dtolstonogova@yandex.ru>
2023-07-15 21:02:44 +0700

    fix checkstyle

--- ewm-service/src/main/java/ru/practicum/explorewithme/event/service/EventService.java
+++ ewm-service/src/main/java/ru/practicum/explorewithme/event/service/EventService.java
@@ -12,14 +12,24 @@ import java.util.List;
 public interface EventService {
 
     List<EventShortDto> findEventsOfUser(Long userId, Integer from, Integer size);
+
     EventFullDto addEvent(Long userId, NewEventDto newEventDto);
+
     EventFullDto findUserEventById(Long userId, Long eventId);
+
     EventFullDto userUpdateEvent(Long userId, Long eventId, UpdateEventUserRequest eventUpdate);
+
     EventFullDto adminUpdateEvent(Long eventId, UpdateEventAdminRequest eventUpdate);
+
     List<EventFullDto> findEventsByAdmin(EventAdminParam eventAdminParam);
+
     List<EventShortDto> findEventsByPublic(EventUserParam eventUserParam, HttpServletRequest request);
+
     EventFullDto findPublishedEventById(Long eventId, HttpServletRequest request);
+
     List<ParticipationRequestDto> findUserEventRequests(Long userId, Long eventId);
+
     EventRequestStatusUpdateResult changeEventRequestsStatus(Long userId, Long eventId, EventRequestStatusUpdateRequest request);
+
     List<Event> findAllByIds(List<Long> ids);
 }
